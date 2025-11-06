1557a4cad7e613fff9e017b975e8baedce9e69bc
celinaluetticke-dev <celina.luetticke@gmail.com>
2025-11-06 19:45:48 +0100

    Praesenzaufgabe 3

--- src/p2/P2_main.java
+++ src/p2/P2_main.java
@@ -1,29 +1,34 @@
 package p2;
 
 public class P2_main {
-    public static void main(String[] args) {
-        
-        int a = 1;
-        int b = 2;
-        int c = 3;
-        int d = 999; 
+	public static void main(String[] args) {
 
-        boolean E1 = (a == b);
-       
-        boolean E2 = (a == b) || (a == c);
-       
-        boolean E3 = (a + b >= c);
-       
-        boolean E4 = (a + b + c >= 100);
+		int a = 1;
+		int b = 2;
+		int c = 3;
+		int d = 999;
 
-       
-        d = 0;
-        if (E1) { d = d + 1; }
-        if (E2) { d = d + 1; }
-        if (E3) { d = d + 1; }
-        if (E4) { d = d + 1; }
+		boolean E1 = (a == b);
 
-        System.out.println("a=" + a + ", b=" + b + ", c=" + c);
-        System.out.println("Erfüllte Eigenschaften: d=" + d);
-    }
+		boolean E2 = (a == b) || (a == c);
+		boolean E3 = (a + b >= c);
+		boolean E4 = (a + b + c >= 100);
+
+		d = 0;
+		if (E1) {
+			d = d + 1;
+		}
+		if (E2) {
+			d = d + 1;
+		}
+		if (E3) {
+			d = d + 1;
+		}
+		if (E4) {
+			d = d + 1;
+		}
+
+		System.out.println("a=" + a + ", b=" + b + ", c=" + c);
+		System.out.println("Erfüllte Eigenschaften: d=" + d);
+	}
 }
--- src/p3/P3_main.java
+++ src/p3/P3_main.java
@@ -1,5 +1,21 @@
 package p3;
 
 public class P3_main {
+    public static void main(String[] args) {
 
+        int z = -12;                
+        if (z < 0) {               
+            z = -z;
+        }
+        System.out.println("Betrag = " + z);
+        
+        z = z * z;                  
+        System.out.println("Quadrat = " + z);            
+      
+        if ((z / 2) * 2 == z) {
+            System.out.println("Gerade");
+        } else {
+            System.out.println("Ungerade");
+        }
+    }
 }
