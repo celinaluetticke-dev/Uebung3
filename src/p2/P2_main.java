e35c76070e5a5ece9871c7b033a70e0db84e4c01
celinaluetticke-dev <celina.luetticke@gmail.com>
2025-11-06 19:26:28 +0100

    Praesenzaufgabe 2

--- src/p2/P2_main.java
+++ src/p2/P2_main.java
@@ -1,5 +1,29 @@
 package p2;
 
 public class P2_main {
+    public static void main(String[] args) {
+        
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 999; 
 
+        boolean E1 = (a == b);
+       
+        boolean E2 = (a == b) || (a == c);
+       
+        boolean E3 = (a + b >= c);
+       
+        boolean E4 = (a + b + c >= 100);
+
+       
+        d = 0;
+        if (E1) { d = d + 1; }
+        if (E2) { d = d + 1; }
+        if (E3) { d = d + 1; }
+        if (E4) { d = d + 1; }
+
+        System.out.println("a=" + a + ", b=" + b + ", c=" + c);
+        System.out.println("Erfüllte Eigenschaften: d=" + d);
+    }
 }
